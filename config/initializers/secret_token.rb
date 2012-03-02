2004ea500ba54929880d3c3b1048f36c872e5044
Brunno dos Santos <squiter85@gmail.com>
2012-03-02 00:42:12 -0300

    New rails application with mongoid

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mongoid::Application.config.secret_token = '89fd5074808cba3c456490f6e268d3b69f655d439667a09dbb2b2a50dcae948fe038ea3daae4660a40730c44024b2ccb45b3d411ab849986ed11ac35e8ad5e4e'
